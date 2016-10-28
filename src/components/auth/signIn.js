2c477c6c263b1a86738514dbf194dec869824fb3
Julian Currie <julian.currie@gmail.com>
2016-10-27 22:30:11 -0700

    Added email/password inputTexts with state

--- src/components/auth/signIn.js
+++ src/components/auth/signIn.js
@@ -2,14 +2,34 @@ import React, {Component} from 'react';
 import {
     View,
     Text,
-    StyleSheet
+    StyleSheet,
+    TextInput,
+    TouchableOpacity
 } from 'react-native';
 
 export default class SignIn extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        };
+    }
+
     render() {
         return (
             <View style={styles.container}>
                 <Text>Sign In</Text>
+                <TextInput
+                    placeholder="Email"
+                    style={styles.input}
+                    underlineColorAndroid={'transparent'}
+                    onChangeText={(text) => this.setState({email: text})}/>
+                <TextInput
+                    placeholder="password"
+                    style={styles.input}
+                    underlineColorAndroid={'transparent'}
+                    onChangeText={(text) => this.setState({password: text})}/>
             </View>
         );
     }
@@ -19,6 +39,16 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        padding: 40
+    },
+    input: {
+        width: 250,
+        height: 50,
+        borderColor: 'black',
+        borderWidth: 1,
+        borderRadius: 5,
+        marginTop: 10,
+        textAlign: 'center'
     }
 });
