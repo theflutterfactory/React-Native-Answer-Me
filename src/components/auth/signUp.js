39b1ca7a26f068ca7f628b4f3576cc81e0d51b6d
Julian Currie <julian.currie@gmail.com>
2016-10-28 03:22:13 -0700

    extracted out styles from SignIn/SignUp.js into styles.js

--- src/components/auth/signUp.js
+++ src/components/auth/signUp.js
@@ -6,6 +6,7 @@ import {
     TextInput,
     TouchableOpacity
 } from 'react-native';
+import styles from '../../styles';
 
 export default class SignUp extends Component {
     constructor(props) {
@@ -43,43 +44,3 @@ export default class SignUp extends Component {
         );
     }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 40,
-        backgroundColor: 'white'
-    },
-    input: {
-        width: 250,
-        height: 50,
-        borderColor: 'black',
-        borderWidth: 1,
-        borderRadius: 5,
-        marginTop: 10,
-        textAlign: 'center'
-    },
-    buttonContainer: {
-        justifyContent: 'center',
-        height: 50,
-        width: 250,
-        marginTop: 10,
-        borderColor: 'black',
-        borderWidth: 1,
-        borderRadius: 5
-    },
-    button: {
-        textAlign: 'center'
-    },
-    link: {
-        marginTop: 10,
-        color: 'blue'
-    },
-    links: {
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-        alignItems: 'center'
-    }
-});
---
+++ src/styles.js
@@ -0,0 +1,41 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 40,
+        backgroundColor: 'white'
+    },
+    input: {
+        width: 250,
+        height: 50,
+        borderColor: 'black',
+        borderWidth: 1,
+        borderRadius: 5,
+        marginTop: 10,
+        textAlign: 'center'
+    },
+    buttonContainer: {
+        justifyContent: 'center',
+        height: 50,
+        width: 250,
+        marginTop: 10,
+        borderColor: 'black',
+        borderWidth: 1,
+        borderRadius: 5
+    },
+    button: {
+        textAlign: 'center'
+    },
+    link: {
+        marginTop: 10,
+        color: 'blue'
+    },
+    links: {
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    }
+});
