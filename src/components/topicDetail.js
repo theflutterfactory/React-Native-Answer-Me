33a7488405d93927a4fe80b01a29f82d3d240c38
Julian Currie <julian.currie@gmail.com>
2016-10-31 21:07:34 -0700

    set up comment listView with user InputText

--- src/components/topicDetail.js
+++ src/components/topicDetail.js
@@ -3,11 +3,26 @@ import {
     View,
     Text,
     TextInput,
-    TouchableOpacity
+    TouchableOpacity,
+    ListView
 } from 'react-native';
 import styles from '../styles';
 
+const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 != r2});
+
 export default class TopicDetail extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            comment: '',
+            dataSource: ds.cloneWithRows([])
+        };
+    }
+
+    postComment() {
+
+    }
+
     render() {
         return (
             <View style={styles.flexContainer}>
@@ -20,6 +35,17 @@ export default class TopicDetail extends Component {
                 <View style={styles.body}>
                     <Text style={styles.detailTitle}>{this.props.title}</Text>
                     <Text style={styles.detailSubtitle}>{this.props.author}</Text>
+                    <TextInput
+                        style={styles.input}
+                        placeholder='Add your thoughts'
+                        onChangeText={(text) => this.setState({comment: text})}
+                        onSubmitEditing={() => this.postComments()}>
+                    </TextInput>
+                    <ListView
+                        style={styles.list}
+                        enableEmptySections={true}
+                        dataSource={this.state.dataSource}
+                        renderRow={(rowData) => this.renderRow(rowData)}/>
                 </View>
             </View>
         );
